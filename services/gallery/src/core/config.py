30a8bb7c1ec9a6f20b688f47e94e84c5e1192230
Ilya Sharov <isharov8520@gmail.com>
2025-05-09 08:30:39 +0300

    Добавил minio

--- services/gallery/src/core/config.py
+++ services/gallery/src/core/config.py
@@ -1,9 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Project(BaseSettings):
+class ProjectConfig(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='project_')
     
+    title: str
+
+class MinioConfig(BaseSettings):
+    model_config =  SettingsConfigDict(env_prefix='minio_')
+    
+    user: str
+    password: str
+    
+    
 class Settings(BaseSettings):
-    pass
+    project: ProjectConfig = ProjectConfig()
+    minio: MinioConfig = MinioConfig()
 
 settings = Settings()
